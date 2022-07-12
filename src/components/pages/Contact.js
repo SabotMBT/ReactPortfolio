ab76ade481990d7e9c7a660abd96321863bcc607
SabotMBT <104104069+SabotMBT@users.noreply.github.com>
2022-07-11 20:26:58 -0600

    finished contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,78 @@
 import React from "react";
 
 export default function Contact() {
-  return console.log("What goes here?");
+  return (
+    <div>
+      <h1>Contact Me!</h1>
+      <div className="row g-3">
+        <div className="input-group col-md-8">
+          <select
+            className="form-select titleSel"
+            id="validationTooltip04"
+            required
+          >
+            <option disabled value="">
+              Title
+            </option>
+            <option>Mr.</option>
+            <option>Ms.</option>
+            <option>Revd.</option>
+            <option>Pr.</option>
+            <option>Br.</option>
+            <option>Fr.</option>
+            <option>Sr.</option>
+            <option>Dr.</option>
+            <option>Prof.</option>
+          </select>
+          <span className="input-group-text">First and Last name</span>
+          <input
+            type="text"
+            aria-label="First name"
+            className="form-control"
+            required
+          ></input>
+          <input
+            type="text"
+            aria-label="Last name"
+            className="form-control"
+            required
+          ></input>
+        </div>
+        <div className="input-group col-md-8">
+          <span className="input-group-text" id="inputGroup-sizing-default">
+            Company or Organization
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Company or Organization name input"
+            aria-describedby="inputGroup-sizing-default"
+            required
+          ></input>
+        </div>
+        <div className="input-group col-md-8">
+          <span className="input-group-text" id="inputGroup-sizing-default">
+            Email Address
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Email Address input"
+            aria-describedby="inputGroup-sizing-default"
+            id="validationDefault03"
+            required
+          ></input>
+        </div>
+        <div className="input-group">
+          <span className="input-group-text">Comments</span>
+          <textarea
+            className="form-control"
+            id="validationDefault05"
+            aria-label="Comments"
+            required
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,21 +4,134 @@ export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <div className="cardcontainer d-flex">
+        <div class="card" style="width: 18rem;">
+          <img
+            src={process.env.PUBLIC_URL + "BachUp.png"}
+            class="card-img-top"
+            alt="Bach That Thang Up"
+          ></img>
+          <div class="card-body">
+            <h5 class="card-title">Bach That Thang Up!</h5>
+            <p class="card-text">
+              An app to search for Artists, get a list of Albums, Top Tracks,
+              and future Events.
+            </p>
+            <a
+              href="https://sabotmbt.github.io/ClassProject1/"
+              class="btn btn-primary"
+            >
+              Deployed Site
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="cardcontainer d-flex">
+        <div class="card" style="width: 18rem;">
+          <img
+            src={process.env.PUBLIC_URL + "techblog.png"}
+            class="card-img-top"
+            alt="Tech Blog"
+          ></img>
+          <div class="card-body">
+            <h5 class="card-title">Tech Blog</h5>
+            <p class="card-text">
+              A simple example of a live blog, with users, authorization
+              functionality, and short 'blog' posting.
+            </p>
+            <a
+              href="https://classtechblog.herokuapp.com/"
+              class="btn btn-primary"
+            >
+              Deployed Site
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="cardcontainer d-flex">
+        <div class="card" style="width: 18rem;">
+          <img
+            src={process.env.PUBLIC_URL + "flores-rosa-emilia.png"}
+            class="card-img-top"
+            alt="..."
+          ></img>
+          <div class="card-body">
+            <h5 class="card-title">Ecommerce Site</h5>
+            <p class="card-text">
+              An Ecommerce site, including items for sale, user accounts,
+              reviews, and a shopping cart.
+            </p>
+            <a
+              href="https://flores-rosa-emilia.herokuapp.com/"
+              class="btn btn-primary"
+            >
+              Deployed Site
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="cardcontainer d-flex">
+        <div class="card" style="width: 18rem;">
+          <img
+            src={process.env.PUBLIC_URL + "notetaker.png"}
+            class="card-img-top"
+            alt="..."
+          ></img>
+          <div class="card-body">
+            <h5 class="card-title">Note Taker</h5>
+            <p class="card-text">
+              A simple app that allows a user to take quick notes. The data
+              persists across sessions and devices.
+            </p>
+            <a
+              href="https://mod11chalnotetaker.herokuapp.com/notes"
+              class="btn btn-primary"
+            >
+              Deployed Site
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="cardcontainer d-flex">
+        <div class="card" style="width: 18rem;">
+          <img
+            src={process.env.PUBLIC_URL + "jate.png"}
+            class="card-img-top"
+            alt="..."
+          ></img>
+          <div class="card-body">
+            <h5 class="card-title">Just Another Text Editor</h5>
+            <p class="card-text">
+              A lightweight Progressive Web Application for writing code.
+            </p>
+            <a
+              href="https://mod19chaljate.herokuapp.com/"
+              class="btn btn-primary"
+            >
+              Deployed Site
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="cardcontainer d-flex">
+        <div class="card" style="width: 18rem;">
+          <img
+            src={process.env.PUBLIC_URL + "BachUp.png"}
+            class="card-img-top"
+            alt="..."
+          ></img>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Deployed Site
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
